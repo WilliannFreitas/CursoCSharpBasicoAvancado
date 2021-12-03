8fcaca27325e2e6a1343f2c459dccd83e70f09a0
WilliannFreitas <willythepower@gmail.com>
2021-12-02 23:55:17 -0300

    Aula do dia 02/12/2021

--- Program.cs
+++ Program.cs
@@ -43,6 +43,9 @@ namespace CursoCSharp
                 {"Membros - Classes e Métodos", Membros.Executar},
                 {"Construtores - Classes e Métodos", Construtores.Executar},
                 {"Métodos com Retorno - Classes e Métodos", MetodosComRetorno.Executar},
+                {"Métodos Estáticos - Classes e Métodos", MetodosEstaticos.Executar},
+                {"Atributos Estáticos - Classes e Métodos", AtributosEstaticos.Executar},
+                {"Desafio Atributos - Classes e Métodos", DesafioAtributo.Executar},
                 
             });
 
